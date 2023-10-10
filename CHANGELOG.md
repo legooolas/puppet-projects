84008b78c7fec050eb55494fb0cdc03c13170255
David Gardner <david.gardner@bristol.ac.uk>
2023-10-10 11:24:46 +0100

    Converted with pdk 2.7.1.
    Add hiera module data for start of lookups of packages/params.

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## Release 4.0.0
+
+**Bugfixes**
+
+        - Update for compatibility with puppetlabs/apache 9.x
+        - Support Ubuntu 22.04 for PHP package list
+
--- manifests/project/apache.pp
+++ manifests/project/apache.pp
@@ -50,15 +50,10 @@ define projects::project::apache (
   if $apache_common['php'] {
     include '::apache::mod::php'
 
-    # TODO : This should be via hiera data rather than params or this sort of hackery, but we're testing here for the moment...
-    $os_id = "${::facts['os']['name']}-${::facts['os']['release']['major']}"
-
-    if $os_id in [ 'Ubuntu-22.04' ] {
-      $php_packages = [ 'php-mysql', 'php-mbstring', 'php-snmp' ]
-    } else {
-      $php_packages = ['php-pdo', 'php-mysql', 'php-mbstring', 'php-snmp']
+    $php_packages = lookup('projects::php_packages', Array, deep, ['php-pdo', 'php-mysql', 'php-mbstring', 'php-snmp'])
+    if !empty($php_packages) {
+      ensure_packages($php_packages)
     }
-    ensure_packages($php_packages)
   }
 
   if $apache_common['mpm'] == 'event' {
