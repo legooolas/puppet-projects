3a133c93b1cdb8d4bacf4e18bb2bdbb0cbbef9b8
David Gardner <david.gardner@bristol.ac.uk>
2024-10-10 11:33:13 +0100

    Actually they should be writable only by the project group, so keep them as 0755 not 0775.

--- manifests/project/apache.pp
+++ manifests/project/apache.pp
@@ -105,7 +105,7 @@ define projects::project::apache (
     owner   => $title,
     group   => $title,
     seltype => 'httpd_config_t',
-    mode    => '0775',
+    mode    => '0755',
     require => File["${::projects::basedir}/${title}/etc"],
   }
 
@@ -122,7 +122,7 @@ define projects::project::apache (
     ensure  => directory,
     owner   => $title,
     group   => $title,
-    mode    => '0775',
+    mode    => '0755',
     seltype => 'cert_t',
     require => File["${::projects::basedir}/${title}/etc"],
   }
@@ -134,7 +134,7 @@ define projects::project::apache (
     ensure  => directory,
     owner   => $title,
     group   => $title,
-    mode    => '0775',
+    mode    => '0755',
     require => File["${::projects::basedir}/${title}/etc/ssl"],
   }
 
